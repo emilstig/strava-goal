34960688547bf0be75508624c6f774b7bbd26a4e
Emil Enestig <emil@oridnary.se>
2020-03-03 15:45:17 +0100

    Refine progress bar styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import "./App.css";
 import getAthleteStats from "./helpers/getAthleteStats";
 import fonts from "./assets/fonts/fonts";
 
+// Strava API
+const token = "b4c9cc03cbe6348b237325cf06838a094552510b";
+const userId = "19649721";
+
+// Dates
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth();
+const currentWeek = getWeek(currentDate);
+const totalWeeks = getISOWeeksInYear(currentDate);
 const months = [
   "January",
   "February",
@@ -22,13 +32,7 @@ const months = [
   "December"
 ];
 
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-const currentMonth = currentDate.getMonth();
-const currentWeek = getWeek(currentDate);
-const totalWeeks = getISOWeeksInYear(currentDate);
-
-const AppStyles = styled.header`
+const Wrapper = styled.div`
   ${fonts}
   * {
     box-sizing: border-box;
@@ -37,12 +41,54 @@ const AppStyles = styled.header`
   * {
     box-sizing: border-box;
   }
+  overflow: hidden;
+  font-family: "Moderat", Helvetica, Arial, sans-serif;
+  background-color: #ffffff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: calc(10px + 2vmin);
+  color: #000000;
+
+  &.animation-step-1 {
+    .Bottom {
+      transform: translateY(0px);
+      &:hover {
+        .ProgressBar {
+          &::after {
+            /* transform: scale(1.1); */
+          }
+        }
+      }
+    }
+    .ProgressBar {
+      &::after {
+        transform: scale(1);
+      }
+    }
+  }
+`;
+
+const Top = styled.header`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 16px;
+`;
+
+const Bottom = styled.footer`
+  transition: transform 0.8s cubic-bezier(0.86, 0, 0.07, 1);
+  transform: translateY(52px);
+  &:hover {
+    /* transform: translateY(0px); */
+  }
 `;
 
 const Labels = styled.div`
   display: flex;
-  width: 100%;
   justify-content: space-between;
+  width: 100%;
   padding: 0 16px;
 `;
 
@@ -76,7 +122,6 @@ const ProgressBar = styled.div`
   align-items: center;
   width: 100%;
   height: 52px;
-
   background: rgba(0, 0, 0, 0.05);
 
   div {
@@ -103,91 +148,105 @@ const ProgressBar = styled.div`
     content: " ";
     position: absolute;
     z-index: 3;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    background: linear-gradient(
-      to right,
-      rgba(255, 255, 255, 1) 0%,
-      rgba(255, 255, 255, 1) ${props => props.goal}%,
-      rgba(20, 20, 20, 1) ${props => props.goal}%,
-      rgba(20, 20, 20, 1) calc(${props => props.goal}% + 2px),
-      rgba(255, 255, 255, 0) calc(${props => props.goal}% + 2px),
-      rgba(255, 255, 255, 0) 100%
-    );
+    width: 2px;
+    /* height: 0; */
+    height: calc(100% + 6px);
+    left: ${props => props.goal}%;
+    top: -3px;
+    background-color: rgba(20, 20, 20, 1);
+    /* transform: translateY(100%); */
+    transform: scale(0);
+    transition: all 0.8s cubic-bezier(0.86, 0, 0.07, 1);
   }
 `;
 
 function App() {
   const [athlete, setAthlete] = useState({});
-  const token = "b4c9cc03cbe6348b237325cf06838a094552510b";
-  const userId = "19649721";
+  const [animation, setAnimation] = useState(0);
+
   useEffect(() => {
+    // Get user stats from Strava
     getAthleteStats(token, userId).then(data => {
       setAthlete(data);
     });
   }, []);
+
+  // Athlete data
   const athleteStats = athlete?.ytd_run_totals;
-  const runningGoalYear = 1000;
-  const runningGoalDistance = (runningGoalYear / totalWeeks) * currentWeek;
-  const runningTarget =
-    (Math.round(runningGoalDistance) / runningGoalYear) * 100;
-  const runningDistanceYear =
+
+  // Running goal
+  const goalDistance = 1000;
+  const goalCurrentDistance = (goalDistance / totalWeeks) * currentWeek;
+  const goalCurrentPercentage =
+    (Math.round(goalCurrentDistance) / goalDistance) * 100;
+
+  // Running current
+  const currentDistance =
     athleteStats && athleteStats.distance
-      ? Math.round(athleteStats.distance / runningGoalYear)
+      ? Math.round(athleteStats.distance / goalDistance)
       : 0;
-  const runningProgress = (runningDistanceYear / runningGoalYear) * 100;
-
+  const currentPercentage = (currentDistance / goalDistance) * 100;
   return (
-    <div className="App">
-      <AppStyles className="App-header">
+    <Wrapper className={animation && "animation-step-" + animation}>
+      <Top className="Top">
         <h1>Running 2020</h1>
-        <div>
-          <Labels>
-            <h2>Distance</h2>
-            <h2>Goal</h2>
-          </Labels>
+      </Top>
 
-          <ProgressBar progress={runningProgress} goal={runningTarget}>
-            <div>
-              {runningDistanceYear > 0 ? (
-                <CountUp end={runningDistanceYear} duration={1} />
-              ) : (
-                "0"
-              )}
-              km
-            </div>
-            <div>{runningGoalYear} km</div>
-          </ProgressBar>
-          <Timeline>
-            {months &&
-              months.map((month, index) => {
-                const monthWeeks = getWeeksInMonth(
-                  new Date(currentYear, index, 1)
-                );
+      <Bottom className="Bottom">
+        <Labels>
+          <h2>Distance</h2>
+          <h2>Goal</h2>
+        </Labels>
 
-                const weekDistance =
-                  (runningGoalYear / totalWeeks) * monthWeeks;
-                const weekWidth =
-                  (Math.round(weekDistance) / runningGoalYear) * 100 + "%";
+        <ProgressBar
+          className="ProgressBar"
+          progress={currentPercentage}
+          goal={goalCurrentPercentage}
+        >
+          <div>
+            {currentDistance > 0 ? (
+              <CountUp
+                end={currentDistance}
+                duration={1}
+                onEnd={() => {
+                  setAnimation(1);
+                }}
+              />
+            ) : (
+              "0"
+            )}
+            km
+          </div>
+          <div>
+            <CountUp end={goalDistance} duration={1} />
+            km
+          </div>
+        </ProgressBar>
+        <Timeline className="Timeline">
+          {months &&
+            months.map((month, index) => {
+              const monthWeeks = getWeeksInMonth(
+                new Date(currentYear, index, 1)
+              );
+              const weekDistance = (goalDistance / totalWeeks) * monthWeeks;
+              const weekWidth =
+                (Math.round(weekDistance) / totalWeeks) * 100 + "%";
 
-                return (
-                  <div
-                    key={"month-" + index}
-                    style={{
-                      opacity: index < currentMonth ? 0.5 : 1,
-                      width: weekWidth
-                    }}
-                  >
-                    {month.substring(0, 3)}
-                  </div>
-                );
-              })}
-          </Timeline>
-        </div>
-      </AppStyles>
-    </div>
+              return (
+                <div
+                  key={"month-" + index}
+                  style={{
+                    opacity: index < currentMonth ? 0.5 : 1,
+                    width: weekWidth
+                  }}
+                >
+                  {month.substring(0, 3)}
+                </div>
+              );
+            })}
+        </Timeline>
+      </Bottom>
+    </Wrapper>
   );
 }
 
