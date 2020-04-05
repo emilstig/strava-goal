b9b4d937ca60b0a3cfe91ab3c4e6f9640a6ea636
Emil Enestig <emil@oridnary.se>
2020-04-05 22:56:19 +0200

    Fix tabs layout issue

--- src/components/ContentTabs/ContentTabs.js
+++ src/components/ContentTabs/ContentTabs.js
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "../UI/Layout/Grid/Container";
 import Row from "../UI/Layout/Grid/Row";
 import Column from "../UI/Layout/Grid/Column";
+import Box from "../UI/Layout/Box";
 import { Tabs, Tab } from "../UI/Tabs/Tabs";
 
 const tabs = [
@@ -28,34 +29,32 @@ const handleOnChange = (event, store, setStore) => {
 
 const ContentTabs = ({ store, setStore }) => {
   return (
-    <Container
-      bg="background"
-      mr={["-16px", null, "0"]}
-      ml={["-16px", null, "-16px"]}
-    >
-      <Row flexDirection="row">
-        <Column width={[12 / 12, null, 3 / 12]}></Column>
-        <Tabs>
-          {tabs &&
-            tabs.length > 0 &&
-            tabs.map((tab, index) => (
-              <Tab
-                key={"tab-" + index}
-                checked={store.tab === tab.value ? true : false}
-              >
-                <input
-                  type="radio"
-                  name="content"
-                  value={tab.value}
-                  autoComplete="off"
+    <Container bg="background">
+      <Box mr={["-16px", null, "0"]} ml={["-16px", null, "-16px"]}>
+        <Row flexDirection="row">
+          <Column width={[12 / 12, null, 3 / 12]}></Column>
+          <Tabs>
+            {tabs &&
+              tabs.length > 0 &&
+              tabs.map((tab, index) => (
+                <Tab
+                  key={"tab-" + index}
                   checked={store.tab === tab.value ? true : false}
-                  onChange={(event) => handleOnChange(event, store, setStore)}
-                />
-                <span>{tab.label}</span>
-              </Tab>
-            ))}
-        </Tabs>
-      </Row>
+                >
+                  <input
+                    type="radio"
+                    name="content"
+                    value={tab.value}
+                    autoComplete="off"
+                    checked={store.tab === tab.value ? true : false}
+                    onChange={(event) => handleOnChange(event, store, setStore)}
+                  />
+                  <span>{tab.label}</span>
+                </Tab>
+              ))}
+          </Tabs>
+        </Row>
+      </Box>
     </Container>
   );
 };
