fb5432dc8109507ee11c6920ce7fd7e9b62b1064
Emil Enestig <emil@oridnary.se>
2020-09-20 18:40:29 +0200

    Refetch paginated activities

--- src/helpers/stravaApi.js
+++ src/helpers/stravaApi.js
@@ -5,14 +5,14 @@ export const getAuthToken = async (client_id, client_secret, code) => {
     method: "POST",
     headers: {
       Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       client_id: client_id,
       client_secret: client_secret,
       code: code,
-      grant_type: "authorization_code"
-    })
+      grant_type: "authorization_code",
+    }),
   };
   let response = await fetch(url, options);
   let data = await response.json();
@@ -30,29 +30,29 @@ export const getRefreshToken = async (
     method: "POST",
     headers: {
       Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       client_id: client_id,
       client_secret: client_secret,
       refresh_token: refresh_token,
-      grant_type: "refresh_token"
-    })
+      grant_type: "refresh_token",
+    }),
   };
   let response = await fetch(url, options);
   let data = await response.json();
   return data;
 };
 
-export const getAthleteProfile = async token => {
+export const getAthleteProfile = async (token) => {
   const url = `https://www.strava.com/api/v3/athlete`;
 
   const options = {
     method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    contentType: "application/json"
+    contentType: "application/json",
   };
   let response = await fetch(url, options);
   let data = await response.json();
@@ -63,29 +63,51 @@ export const getAthleteStats = async (token, userId, yearTimestamp) => {
   const options = {
     method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    contentType: "application/json"
+    contentType: "application/json",
   };
-  const statsUrl = `https://www.strava.com/api/v3/athletes/${userId}/stats`;
-  const activitiesUrl = `https://www.strava.com/api/v3/athlete/activities?per_page=100&after=${yearTimestamp}`;
+  const url = `https://www.strava.com/api/v3/athletes/${userId}/stats`;
 
   try {
     let [athleteStats, athleteActivities] = await Promise.all([
-      fetch(statsUrl, options)
-        .then(response =>
-          response.text().then(text => (text ? JSON.parse(text) : {}))
+      fetch(url, options)
+        .then((response) =>
+          response.text().then((text) => (text ? JSON.parse(text) : {}))
         )
-        .then(data => data),
-      fetch(activitiesUrl, options)
-        .then(response =>
-          response.text().then(text => (text ? JSON.parse(text) : {}))
-        )
-        .then(data => data)
+        .then((data) => data),
+      getAllYearActivities(1, options, yearTimestamp),
     ]);
 
     return { athleteStats, athleteActivities };
   } catch (err) {
     console.warn(err);
+  }
+};
+
+const getActivities = async function (page = 1, options, yearTimestamp) {
+  const url = `https://www.strava.com/api/v3/athlete/activities?per_page=100&page=${page}&after=${yearTimestamp}`;
+  var apiResults = await fetch(url, options)
+    .then((response) =>
+      response.text().then((text) => (text ? JSON.parse(text) : []))
+    )
+    .then((data) => {
+      console.log("getAthleteStats -> data", data);
+      return data;
+    });
+
+  return apiResults;
+};
+
+const getAllYearActivities = async function (page = 1, options, yearTimestamp) {
+  const results = await getActivities(page, options, yearTimestamp);
+  console.log("Retreiving data from API for page : " + page);
+  console.log("results.length", results.length);
+  if (results.length >= 100) {
+    return results.concat(
+      await getAllYearActivities(page + 1, options, yearTimestamp)
+    );
+  } else {
+    return results;
   }
 };
